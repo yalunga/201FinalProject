54178d55189c6849a0472cb65f090978c6727dec
KatieSing <ksing@usc.edu>
2018-11-01 16:49:53 -0700

    finished logic for checkin

--- src/finalProject/Attendance.java
+++ src/finalProject/Attendance.java
@@ -1,12 +1,13 @@
 package finalProject;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
 /**
  * Servlet implementation class Attendance
@@ -35,6 +36,9 @@ public class Attendance extends HttpServlet {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		ResultSet rs;
+		ResultSet rs2;
+		ResultSet rs3;
+		
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		if(studentID != null && studentID != ""
@@ -56,66 +60,88 @@ public class Attendance extends HttpServlet {
 				String date = dateFull.substring(0,10);
 				String time = dateFull.substring(11);
 				
-				
-				Class.forName("com.mysql.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://localhost/askUSC?user=root&password=root&useSSL=false");
-				
-				
-				ps = conn.prepareStatement(
-						"SELECT * FROM User WHERE studentID=?"
-						);
-				ps.setString(1, studentID);
-				rs = ps.executeQuery();
-				
-				if(rs.next()) { //Student exists
-					System.out.println("found student");					
-					//check if today's date equals a date in the table DaysLectureMeets
-					//if datetimecorrect { if location correct { }
-					PreparedStatement ps2 = conn.prepareStatement(
-							"IF EXISTS (SELECT * FROM DaysLectureMeets WHERE lectureUUID = ? AND lectureDate = ?)"
-							+ "SELECT * FROM Lecture WHERE lectureUUID = ? AND ? BETWEEN startTime AND AddTime(startTime, '00:10:00')"
+				try {	
+
+					Class.forName("com.mysql.jdbc.Driver");
+					conn = DriverManager.getConnection("jdbc:mysql://localhost/askUSC?user=root&password=root&useSSL=false");
+					
+					
+					ps = conn.prepareStatement(
+							"SELECT * FROM User WHERE studentID=?"
 							);
-					ps2.setString(1, lectureID);
-				    ps2.setString(2, date);
-				    ps2.setString(3, lectureID);
-				    ps2.setString(4, time);
-					rs2 = ps2.executeQuery();
-
-					if(rs2.next()) {	
-						PreparedStatement ps3 = conn.prepareStatement(
-						"IF EXISTS (SELECT * FROM Lecture WHERE longitude = ? AND latitude = ?)"
-						+ "INSERT INTO AttendanceRecord (studentID, lectureUUID, lectureDate) VALUES (?, ?, ?)"
-					    );
-						ps3.setString(1, longitude);
-					    ps3.setString(2, latitude);
-						ps3.setString(3, studentID);
-						ps3.setString(4, lectureID);
-						ps3.setString(5, date);
-
-						int result = ps3.executeUpdate();
-						System.out.println(result);
-						if(result > 0) {
-							response.getWriter().write("Checked In Successfully.");
-						}
-						else { //location incorrect
+					ps.setString(1, studentID);
+					rs = ps.executeQuery();
+					
+					if(rs.next()) { //Student exists
+						System.out.println("Success: student exists.");					
+						//check if today's date equals a date in the table DaysLectureMeets
+						//if datetimecorrect { if location correct { }
+						PreparedStatement ps2 = conn.prepareStatement(
+								"IF EXISTS (SELECT * FROM DaysLectureMeets WHERE lectureUUID = ? AND lectureDate = ?)"
+								+ "SELECT * FROM Lecture WHERE lectureUUID = ? AND ? BETWEEN startTime AND endTime"
+								);
+						//AddTime(startTime, '00:10:00')
+						ps2.setString(1, lectureID);
+					    ps2.setString(2, date);
+					    ps2.setString(3, lectureID);
+					    ps2.setString(4, time);
+						rs2 = ps2.executeQuery();
+						
+						if(rs2.next()) {	//class is in session
+							PreparedStatement ps3 = conn.prepareStatement(
+									"SELECT * FROM Lecture WHERE lectureUUID = ? AND longitude = ? AND latitude = ?)"
+							);
+							ps3.setString(1, lectureID);
+							ps3.setString(2, longitude);
+						    ps3.setString(3, latitude);
+						    rs3 = ps3.executeQuery();
+						    if(rs3.next()) {    //user is in the correct location
+						    	
+						    	PreparedStatement ps4 = conn.prepareStatement(
+						    			"IF EXISTS (SELECT * FROM Lecture WHERE lectureUUID = ? AND ? BETWEEN startTime AND AddTime(startTime, '00:10:00'))"
+						    			+ "INSERT INTO AttendanceRecord (studentID, lectureUUID, lectureDate) VALUES (?, ?, ?)"
+						    	); //if inserted, it meant that the user checked in during the 10 minute time slot
+						    	ps4.setString(1, lectureID);
+							    ps4.setString(2, time);
+						    	ps4.setString(3, studentID);
+						    	ps4.setString(4, lectureID);
+						    	ps4.setString(5, date);
+						    	
+						    	ps4.executeUpdate();
+						    	System.out.println("3: User is in the correct location within the classtime.");
+						    	response.getWriter().write("3");
+						    	
+						    }
+						    else {
+						    	System.out.println("2: You must be in the classroom to type questions.");
+						    	response.getWriter().write("2");
+						    }
+							ps3.close();
 							
+						} //endif (datetime was correct)
+						
+						else { //datetime incorrect
 							//TODO:
 							//output some error
-							System.out.println("send to frontend: Wrong location: You must be in the classroom!");
-						} 
-						ps3.close();
+							System.out.println("1: Class is not in session.");
+							response.getWriter().write("1");
+						}
+						ps2.close();
 						
-					} //endif (datetime was correct)
-					
-					else { //datetime incorrect
-						//TODO:
-						//output some error
-						System.out.println("send to frontend: Class is not in session.");
+					} //endif (student existed)
+					ps.close();
+				}catch(SQLException sqle) {
+					System.out.println(sqle.getMessage());
+				} catch(ClassNotFoundException cnfe) {
+					System.out.println(cnfe.getMessage());
+				} finally {
+					try {
+						if(ps != null) { ps.close(); }
+						if(conn != null) { conn.close(); }
+					} catch(SQLException sqle) {
+						System.out.println(sqle.getMessage());
 					}
-					ps2.close();
-					
-				} //endif (student existed)
-				ps.close();
+				}
 			}
 			
 			

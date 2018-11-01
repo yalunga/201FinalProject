77fd29611e61e84efe99d41fbf40156e673f0287
KatieSing <ksing@usc.edu>
2018-11-01 11:59:57 -0700

    attendance servlet 1/2 done

---
+++ src/finalProject/Attendance.java
@@ -0,0 +1,139 @@
+package finalProject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * Servlet implementation class Attendance
+ */
+@WebServlet("/Attendance")
+public class Attendance extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Attendance() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		String requestType = request.getParameter("requestType");
+		String studentID = request.getParameter("studentID");
+		String lectureID = request.getParameter("lectureID");
+
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs;
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		if(studentID != null && studentID != ""
+			&& lectureID != null && lectureID != "") { //returns string upon success, nothing upon fail
+			
+			// ************************* CHECK IN *************************************
+			
+			if (requestType == "checkIn") {
+				Date dateRaw = new Date();
+			
+			
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+				simpleDateFormat.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+				System.out.println(simpleDateFormat.format(dateRaw));
+				String dateFull = simpleDateFormat.format(dateRaw);
+				String date = dateFull.substring();
+				
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/askUSC?user=root&password=root&useSSL=false");
+				ps = conn.prepareStatement(
+						"SELECT * FROM User WHERE studentID=?"
+						);
+				ps.setString(1, studentID);
+					rs = ps.executeQuery();
+					if(rs.next()) {
+						System.out.println("found student");
+						PreparedStatement ps2 = conn.prepareStatement(
+								"INSERT INTO AttendanceRecord (studentID, lectureUUID, lectureDate) VALUES (?, ?, ?)"
+								);
+						ps2.setString(1, studentID);
+						ps2.setString(2, lectureID);
+						ps2.setString(3, date);
+						
+						int result = ps2.executeUpdate();
+						System.out.println(result);
+						if(result > 0) {
+							response.getWriter().write("Checked In Successfully.");
+						}
+						ps2.close();
+					}
+			}
+			
+			
+			// ************************* GET HISTORY *************************************
+			
+			else if (requestType == "getHistory") {
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					conn = DriverManager.getConnection("jdbc:mysql://localhost/askUSC?user=root&password=root&useSSL=false");
+					ps = conn.prepareStatement(
+							"SELECT c.department, c.classNumber, c.classDescription, u.fullName "
+							+ "FROM lectureRegistration lr"
+							+ "INNER JOIN Lecture l "
+							+ "ON lr.lectureUUID = l.lectureUUID "
+							+ "INNER JOIN Class c "
+							+ "ON l.classID = c.classID"
+							+ "INNER JOIN User u"
+							+ "ON c.instructorID = u.instructorID "
+							+ "WHERE studentID = ?"
+							);
+					ArrayList<Lecture> lectures = new ArrayList<Lecture>();
+					ps.setString(1, studentID);
+					if(studentID != null && studentID != "") {
+						rs = ps.executeQuery();
+						while(rs.next()) {
+							String dept = rs.getString("c.department");
+							String num = rs.getString("c.classNumber");
+							String des = rs.getString("c.classDescription");
+							String instr = rs.getString("u.fullName");
+							Lecture temp = new Lecture(dept, num, des, instr); 
+							lectures.add(temp);
+						}
+					}
+					String json = new Gson().toJson(lectures);
+					response.getWriter().write(json);
+				} catch(SQLException sqle) {
+					System.out.println(sqle.getMessage());
+				} catch(ClassNotFoundException cnfe) {
+					System.out.println(cnfe.getMessage());
+				} finally {
+					try {
+						if(ps != null) { ps.close(); }
+						if(conn != null) { conn.close(); }
+					} catch(SQLException sqle) {
+						System.out.println(sqle.getMessage());
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

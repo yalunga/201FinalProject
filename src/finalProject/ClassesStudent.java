02549e4cba57efeeab05f60bfe70e393ec38b9bd
yalunga <yalung@usc.edu>
2018-11-18 10:10:03 -0700

    fixed register

--- src/finalProject/ClassesStudent.java
+++ src/finalProject/ClassesStudent.java
@@ -1,11 +1,18 @@
 package finalProject;
 
 import java.io.IOException;
-import java.sql.*;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,25 +20,25 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 /**
- * Servlet implementation class Classes
+ * Servlet implementation class ClassesStudent
  */
-@WebServlet("/Classes")
 public class ClassesStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
     public ClassesStudent() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		String requestType = request.getParameter("requestType");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+    	String requestType = request.getParameter("requestType");
 		String studentID = request.getParameter("studentID");
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -46,7 +53,7 @@ public class ClassesStudent extends HttpServlet {
 			String lectureID = request.getParameter("lectureID");
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
-				conn =  DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net:3306/?user=bcbc373fe829dc&useSSL=false");
+				conn =  DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net:3306/heroku_6033235a05719ed?user=bcbc373fe829dc&password=345a5a30&useSSL=false");
 				ps =  conn.prepareStatement(
 						"SELECT * FROM Lecture WHERE lectureUUID=?"
 						);
@@ -56,7 +63,7 @@ public class ClassesStudent extends HttpServlet {
 					if(rs.next()) {
 						System.out.println("found a lecture");
 						PreparedStatement ps2 =  conn.prepareStatement(
-								"INSERT IF NOT EXISTS INTO LectureRegistration (userID, lectureUUID) VALUES (?, ?)"
+								"INSERT INTO LectureRegistration (userID, lectureUUID) VALUES (?, ?)"
 								);
 						ps2.setString(1, studentID);
 						ps2.setString(2, lectureID);
@@ -67,12 +74,15 @@ public class ClassesStudent extends HttpServlet {
 							stmt.setString(2, lectureID);
 							ResultSet res = stmt.executeQuery();
 							response.getWriter().write("Added");
+						} else {
+							response.getWriter().write("Already Registered");
 						}
 						ps2.close();
-					}
-				} else {
 					
-					response.getWriter().write("Lecture Not Found");
+					} else {
+						
+						response.getWriter().write("Failed");
+					}
 				}
 				ps.close();
 			} catch(SQLException sqle) {
@@ -95,7 +105,7 @@ public class ClassesStudent extends HttpServlet {
 			String lectureID = request.getParameter("lecutreID");
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://localhost/askUSC?user=root&password=root&useSSL=false");
+				conn = DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net:3306/heroku_6033235a05719ed?user=bcbc373fe829dc&password=345a5a30&useSSL=false");
 				ps = conn.prepareStatement(" DELETE FROM LectureRegistration WHERE studentID = ? AND LectureUUID = ?");
 				ps.setString(1, studentID);
 				ps.setString(2, lectureID);
@@ -141,7 +151,7 @@ public class ClassesStudent extends HttpServlet {
 			System.out.println("Reached getClasses");
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
-				conn = DriverManager.getConnection("jdbc:mysql://localhost/askUSC?user=root&password=root&useSSL=false");
+				conn = DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net:3306/heroku_6033235a05719ed?user=bcbc373fe829dc&password=345a5a30&useSSL=false");
 				ps = conn.prepareStatement(
 						"SELECT c.department, c.classNumber, c.classDescription, u.fullName, l.latitude, l.longitude, l.startTime, l.endTime, l.meetingDaysOfWeek, l.lectureUUID FROM lectureRegistration lr INNER JOIN Lecture l ON lr.lectureUUID = l.lectureUUID INNER JOIN Class c ON l.classID = c.classID INNER JOIN User u ON l.instructorID = u.instructorID WHERE lr.userID = ?"
 						);
